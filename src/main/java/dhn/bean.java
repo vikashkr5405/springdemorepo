96846314af6967b0b4521bdbd5d317361e2dcaa1
Mukesh Kumar <you@example.com>
2023-02-24 17:00:05 +0530

    beans

---
+++ src/main/java/dhn/bean.java
@@ -0,0 +1,25 @@
+package dhn;
+public class bean {
+int id;
+String name;
+public bean()
+{
+	
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public double amount()
+{
+	return 12500.25;
+}
+}
---
+++ src/main/java/dhn/beanrun.java
@@ -0,0 +1,13 @@
+package dhn;
+
+public class beanrun {
+public static void main(String args[])
+{
+	bean b=new bean();
+	b.setId(45);
+	b.setName("vikash kumar gupta");
+	System.out.println("id is "+b.getId());
+	System.out.println("name is "+b.getName());
+	System.out.println("amount is "+b.amount());
+}
+}
